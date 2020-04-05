70f8bbc0b55760d637080482cff0877335c79117
Tejen Shrestha <shrestha.tejen@gmail.com>
2020-04-05 17:09:32 -0400

    trim string.

--- getHash.js
+++ getHash.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 const text = fs.readFileSync('hash.txt', 'utf8')
-const hash = text.split(' | ')[2].replace(' -->', '')
+const hash = text.split(' | ')[2].replace(' -->', '').trim()
 
 const hashjs = `window.nytBuildHash = '${hash}'`
 fs.writeFileSync('hash.js', hashjs)
--- hash.js
+++
@@ -1,2 +0,0 @@
-window.nytBuildHash = '658e995d33df44fc774530530466688528281a08
-'
