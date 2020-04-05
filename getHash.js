7e2325c042af8fcdeb1239f13dedb90f7945041b
Tejen Shrestha <shrestha.tejen@gmail.com>
2020-04-05 16:53:47 -0400

    make use of hash.

--- getHash.js
+++ getHash.js
@@ -2,5 +2,5 @@ const fs = require('fs')
 
 const text = fs.readFileSync('hash.txt', 'utf8')
 const hash = text.split(' | ')[2].replace(' -->', '')
-const hashjs = `const hash = ${hash}`
+const hashjs = `const hash = '${hash}' // generated ${new Date().toLocaleString()}`
 fs.writeFileSync('hash.js', hashjs)
