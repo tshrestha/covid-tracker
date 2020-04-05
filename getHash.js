fcf7bacb8cdb7e3bccd8368b89abd56e7cc81833
Tejen Shrestha <shrestha.tejen@gmail.com>
2020-04-05 16:58:26 -0400

    Updated getHash.

--- getHash.js
+++ getHash.js
@@ -2,5 +2,7 @@ const fs = require('fs')
 
 const text = fs.readFileSync('hash.txt', 'utf8')
 const hash = text.split(' | ')[2].replace(' -->', '')
-const hashjs = `const hash = '${hash}' // generated ${new Date().toLocaleString()}`
+
+let hashjs = `// Generated ${new Date().toLocaleString()}\n`
+hashjs += `const hash = '${hash}' // generated ${new Date().toLocaleString()}`
 fs.writeFileSync('hash.js', hashjs)
