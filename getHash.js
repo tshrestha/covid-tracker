65e8ca09cfa24f718481b7938175ea089e93edcf
Tejen Shrestha <shrestha.tejen@gmail.com>
2020-04-05 22:08:44 -0400

    Added timestamp.

--- getHash.js
+++ getHash.js
@@ -3,8 +3,9 @@ const fs = require('fs')
 const text = fs.readFileSync('hash.txt', 'utf8')
 const hash = text.split(' | ')[2].replace(' -->', '').trim()
 
-const hashjs = `
-// Generated ${new Date().toLocaleString()}
+const timestamp = new Date().toLocaleString()
+const hashjs = `// Generated ${timestamp}
+window.updateTimestamp = '${timestamp}'
 window.nytBuildHash = '${hash}'
 `
 fs.writeFileSync('hash.js', hashjs)
